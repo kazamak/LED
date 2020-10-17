c0a0deefef81dd4dde355cb1086204aa27f061ea
風間和彦 <kazamak@gmail.com>
2020-10-17 15:40:16 +0900

    test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -56,9 +56,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(41, 108);
+            this.button2.Location = new System.Drawing.Point(97, 73);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(81, 23);
             this.button2.TabIndex = 4;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
